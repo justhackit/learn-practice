8ba290d6672a223e512b98f5beda0670a4c2a62c
Ajay Ed <ajay.nextstep@gmail.com>
2023-08-24 17:44:33 -0500

    migrating to cloudlifter/mono-cloudlifter/ideas/AWSomeUI/awsomeui-web

--- learn-ui/awsomeui-cloudscape/src/store.tsx
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import authSlice from './state-slices/authSlice';
-
-export const store = configureStore({
-  reducer: {
-    userAuth: authSlice,
-  },
-  devTools: true,
-});
